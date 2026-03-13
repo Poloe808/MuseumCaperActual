3210dcc5844650bb461c4ee3c829d30b003d0f93
Lucaspab <lucaspab28@up.edu>
2026-03-12 20:30:44 -0700

    did the first JUnit testGetBoard()
    did the first test, fyi this took way to long was super confused but finally figured it out. also apparently the height is 13.

--- app/src/test/java/edu/up/cs301/museumCaper/MuseumCaperStateTest.java
+++ app/src/test/java/edu/up/cs301/museumCaper/MuseumCaperStateTest.java
@@ -1,11 +1,22 @@
 package edu.up.cs301.museumCaper;
 
+
 import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MuseumCaperStateTest extends TestCase {
 
     public void testGetBoard() {
-        
+        MuseumCaperState testBoard = new MuseumCaperState();
+        List<List<MapTile>> board = testBoard.getBoard();
+        // if it exist
+        assertNotNull(board);
+        // how tall
+        assertEquals(13, board.get(0).size());
+        // how width
+        assertEquals(12,board.size());
     }
 
     public void testGetTurn() {
@@ -18,6 +29,8 @@ public class MuseumCaperStateTest extends TestCase {
     }
 
     public void testSetTurn() {
+        MuseumCaperState setTurn = new MuseumCaperState();
+
     }
 
     public void testSetVisible() {
