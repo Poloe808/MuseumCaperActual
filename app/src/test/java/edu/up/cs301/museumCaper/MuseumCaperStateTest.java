efbc70b8212b88a48154e0ef993ed6941e2ad152
Alberto Lucero <luceroa29@up.edu>
2026-03-12 20:44:39 -0700

    Junit tests

--- app/src/test/java/edu/up/cs301/museumCaper/MuseumCaperStateTest.java
+++ app/src/test/java/edu/up/cs301/museumCaper/MuseumCaperStateTest.java
@@ -2,37 +2,93 @@ package edu.up.cs301.museumCaper;
 
 import junit.framework.TestCase;
 
+import java.util.List;
+import java.util.Map;
+
 public class MuseumCaperStateTest extends TestCase {
 
+    public void testConstructor() {
+        MuseumCaperState test = new MuseumCaperState();
+        int turn = test.getTurn();
+        assertEquals(0,turn);
+    }
+
+    public void testCopyConstructor() {
+        MuseumCaperState test = new MuseumCaperState();
+    }
+
     public void testGetBoard() {
-        
+        MuseumCaperState testBoard = new MuseumCaperState();
+        List<List<MapTile>> board = testBoard.getBoard();
+        //if it exist
+        assertNotNull(board);
+        // how tall
+        assertEquals(13, board.get(0).size());
+        // how width
+        assertEquals(12,board.size());
     }
 
     public void testGetTurn() {
+        MuseumCaperState test = new MuseumCaperState();
+        int p1 = test.getTurn();
+        assertEquals(0,p1);
     }
 
     public void testGetIsVisible() {
+        MuseumCaperState test = new MuseumCaperState();
+        boolean p1 = test.getIsVisible();
+        assertFalse(p1);
     }
 
     public void testGetStolenPaintings() {
+        MuseumCaperState test = new MuseumCaperState();
+        int stolen = test.getStolenPaintings();
+        assertEquals(0,stolen);
     }
 
     public void testSetTurn() {
+        MuseumCaperState test = new MuseumCaperState();
+        test.setTurn(0);
+        assertEquals(0,test.getTurn());
     }
 
     public void testSetVisible() {
+        MuseumCaperState test = new MuseumCaperState();
+        test.setVisible(false);
+        boolean test2 = test.getIsVisible();
+        assertFalse(test2);
     }
 
     public void testSetStolenPaintings() {
+        MuseumCaperState test = new MuseumCaperState();
+        test.setStolenPaintings(0);
+        int stolen = test.getStolenPaintings();
+        assertEquals(0,stolen);
     }
 
     public void testTestToString() {
+        MuseumCaperState test = new MuseumCaperState();
+        String string = test.toString();
+        assertEquals("Turn: " + 0 + "Is Thief Visible: "
+                + false + "Amount of Stolen Paintings: " + 0 +
+                "\nThief Escaped: " + false +
+                "\nThief Caught: " + false, string);
     }
 
     public void testSetTopWall() {
+        MuseumCaperState test = new MuseumCaperState();
+        MapTile tile = new MapTile();
+        tile.setTopWall(true);
+        boolean tileCheck = tile.getTopWall();
+        assertTrue(tileCheck);
     }
 
     public void testSetLeftWall() {
+        MuseumCaperState test = new MuseumCaperState();
+        MapTile tile = new MapTile();
+        tile.setLeftWall(true);
+        boolean tileCheck = tile.getLeftWall();
+        assertTrue(tileCheck);
     }
 
     public void testSetWalls() {
