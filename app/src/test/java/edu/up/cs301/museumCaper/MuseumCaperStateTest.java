7391e92c96030d4e8c4c54b1282da040c8dae3c9
Lucaspab <lucaspab28@up.edu>
2026-03-12 20:37:42 -0700

    Update MuseumCaperStateTest.java

--- app/src/test/java/edu/up/cs301/museumCaper/MuseumCaperStateTest.java
+++ app/src/test/java/edu/up/cs301/museumCaper/MuseumCaperStateTest.java
@@ -1,22 +1,11 @@
 package edu.up.cs301.museumCaper;
 
-
 import junit.framework.TestCase;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MuseumCaperStateTest extends TestCase {
 
     public void testGetBoard() {
-        MuseumCaperState testBoard = new MuseumCaperState();
-        List<List<MapTile>> board = testBoard.getBoard();
-        // if it exist
-        assertNotNull(board);
-        // how tall
-        assertEquals(13, board.get(0).size());
-        // how width
-        assertEquals(12,board.size());
+        
     }
 
     public void testGetTurn() {
@@ -29,8 +18,6 @@ public class MuseumCaperStateTest extends TestCase {
     }
 
     public void testSetTurn() {
-        MuseumCaperState setTurn = new MuseumCaperState();
-
     }
 
     public void testSetVisible() {
