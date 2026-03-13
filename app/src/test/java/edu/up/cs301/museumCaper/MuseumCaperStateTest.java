16b3aec3e310a57020a9df5566990f2f468057bc
Lucaspab <lucaspab28@up.edu>
2026-03-12 19:01:07 -0700

    JUnit4 test created
    created the JUnit test for the museumcaperstate class

---
+++ app/src/test/java/edu/up/cs301/museumCaper/MuseumCaperStateTest.java
@@ -0,0 +1,40 @@
+package edu.up.cs301.museumCaper;
+
+import junit.framework.TestCase;
+
+public class MuseumCaperStateTest extends TestCase {
+
+    public void testGetBoard() {
+        
+    }
+
+    public void testGetTurn() {
+    }
+
+    public void testGetIsVisible() {
+    }
+
+    public void testGetStolenPaintings() {
+    }
+
+    public void testSetTurn() {
+    }
+
+    public void testSetVisible() {
+    }
+
+    public void testSetStolenPaintings() {
+    }
+
+    public void testTestToString() {
+    }
+
+    public void testSetTopWall() {
+    }
+
+    public void testSetLeftWall() {
+    }
+
+    public void testSetWalls() {
+    }
+}
