c57cede95ecb2be6668db7a8a6acfc275967120d
Alberto Lucero <luceroa29@up.edu>
2026-03-19 09:30:02 -0700

    Connected board to GUI, removed points from caperState

--- app/src/main/java/edu/up/cs301/museumCaper/MuseumCaperHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/museumCaper/MuseumCaperHumanPlayer.java
@@ -21,52 +21,53 @@ import android.view.View.OnClickListener;
  */
 public class MuseumCaperHumanPlayer extends GameHumanPlayer implements OnClickListener {
 
-	/* instance variables */
-	
-	// The TextView the displays the current counter value
-	private TextView counterValueTextView;
-	
-	// the most recent game state, as given to us by the CounterLocalGame
-	private MuseumCaperState state;
-	
-	// the android activity that we are running
-	private GameMainActivity myActivity;
-	
-	/**
-	 * constructor
-	 * @param name
-	 * 		the player's name
-	 */
-	public MuseumCaperHumanPlayer(String name) {
-		super(name);
-	}
+    /* instance variables */
 
-	/**
-	 * Returns the GUI's top view object
-	 * 
-	 * @return
-	 * 		the top object in the GUI's view heirarchy
-	 */
-	public View getTopView() {
-		return myActivity.findViewById(R.id.top_gui_layout);
-	}
-	
-	/**
-	 * sets the counter value in the text view
-	 */
-	protected void updateDisplay() {
-		// set the text in the appropriate widget
-		//counterValueTextView.setText("" + state.getCounter());
-	}
+    // The TextView the displays the current counter value
+    private TextView counterValueTextView;
 
-	/**
-	 * this method gets called when the user clicks the '+' or '-' button. It
-	 * creates a new CounterMoveAction to return to the parent activity.
-	 * 
-	 * @param button
-	 * 		the button that was clicked
-	 */
-	public void onClick(View button) {
+    // the most recent game state, as given to us by the CounterLocalGame
+    private MuseumCaperState state;
+
+    // the android activity that we are running
+    private GameMainActivity myActivity;
+
+    /**
+     * constructor
+     *
+     * @param name the player's name
+     */
+    public MuseumCaperHumanPlayer(String name) {
+        super(name);
+    }
+
+    /**
+     * Returns the GUI's top view object
+     *
+     * @return the top object in the GUI's view heirarchy
+     */
+    public View getTopView() {
+
+        return myActivity.findViewById(R.id.myBoard);
+    }
+
+
+    /**
+     * sets the counter value in the text view
+     */
+    protected void updateDisplay() {
+        // set the text in the appropriate widget
+        //counterValueTextView.setText("" + state.getCounter());
+    }
+
+    /**
+     * this method gets called when the user clicks the '+' or '-' button. It
+     * creates a new CounterMoveAction to return to the parent activity.
+     *
+     * @param button the button that was clicked
+     */
+    public void onClick(View button) {
+        /*
 		// if we are not yet connected to a game, ignore
 		if (game == null) return;
 
@@ -86,40 +87,41 @@ public class MuseumCaperHumanPlayer extends GameHumanPlayer implements OnClickLi
 		}
 		
 		game.sendAction(action); // send action to the game
-	}// onClick
-	
-	/**
-	 * callback method when we get a message (e.g., from the game)
-	 * 
-	 * @param info
-	 * 		the message
-	 */
-	@Override
-	public void receiveInfo(GameInfo info) {
-		// ignore the message if it's not a CounterState message
-		if (!(info instanceof MuseumCaperState)) return;
-		
-		// update our state; then update the display
-		this.state = (MuseumCaperState)info;
-		updateDisplay();
-	}
-	
-	/**
-	 * callback method--our game has been chosen/rechosen to be the GUI,
-	 * called from the GUI thread
-	 * 
-	 * @param activity
-	 * 		the activity under which we are running
-	 */
-	public void setAsGui(GameMainActivity activity) {
-		
-		// remember the activity
-		this.myActivity = activity;
-		
-	    // Load the layout resource for our GUI
-		activity.setContentView(R.layout.counter_human_player);
+
+         */
+    }// onClick
+
+    /**
+     * callback method when we get a message (e.g., from the game)
+     *
+     * @param info the message
+     */
+    @Override
+    public void receiveInfo(GameInfo info) {
+        // ignore the message if it's not a CounterState message
+        if (!(info instanceof MuseumCaperState)) return;
+
+        // update our state; then update the display
+        this.state = (MuseumCaperState) info;
+        updateDisplay();
+    }
+
+    /**
+     * callback method--our game has been chosen/rechosen to be the GUI,
+     * called from the GUI thread
+     *
+     * @param activity the activity under which we are running
+     */
+    public void setAsGui(GameMainActivity activity) {
+
+        // remember the activity
+        this.myActivity = activity;
+
+		//Load the layout resource for our GUI
+		activity.setContentView(R.layout.activity_main);
 		
 		// make this object the listener for both the '+' and '-' 'buttons
+        /*
 		Button plusButton = (Button) activity.findViewById(R.id.plusButton);
 		plusButton.setOnClickListener(this);
 		Button minusButton = (Button) activity.findViewById(R.id.minusButton);
@@ -133,8 +135,14 @@ public class MuseumCaperHumanPlayer extends GameHumanPlayer implements OnClickLi
 		// the state from the game so that the GUI values are updated
 		if (state != null) {
 			receiveInfo(state);
-		}
-	}
+
+
+
+
+    }
+
+         */
+}
 
 }// class CounterHumanPlayer
 
