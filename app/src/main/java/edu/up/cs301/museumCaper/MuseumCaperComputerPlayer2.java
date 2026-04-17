7186d918743696530bc1bac96d8bfd0f3766317a
Poloe808 <29poloe@gmail.com>
2026-04-17 06:04:53 -0700

    (´;ω;｀)

--- app/src/main/java/edu/up/cs301/museumCaper/MuseumCaperComputerPlayer2.java
+++ app/src/main/java/edu/up/cs301/museumCaper/MuseumCaperComputerPlayer2.java
@@ -1,7 +1,11 @@
 package edu.up.cs301.museumCaper;
 
 import edu.up.cs301.GameFramework.GameMainActivity;
+import edu.up.cs301.GameFramework.actionMessage.GameAction;
 import edu.up.cs301.GameFramework.infoMessage.GameInfo;
+import edu.up.cs301.GameFramework.players.GameComputerPlayer;
+import edu.up.cs301.GameFramework.utilities.Tickable;
+
 import android.app.Activity;
 import android.os.Handler;
 import android.util.Log;
@@ -27,11 +31,13 @@ import java.util.Random;
  *
 * @version September 2013
 */
-public class MuseumCaperComputerPlayer2 extends MuseumCaperComputerPlayer1 {
+public class MuseumCaperComputerPlayer2 extends GameComputerPlayer implements Tickable {
 	
 	/*
 	 * instance variables
 	 */
+    private MuseumCaperState state;
+    private Random rng = new Random();
 	
 	/**
 	 * constructor
@@ -51,9 +57,79 @@ public class MuseumCaperComputerPlayer2 extends MuseumCaperComputerPlayer1 {
      */
 	@Override
 	protected void receiveInfo(GameInfo info) {
-        // perform superclass behavior
-        super.receiveInfo(info);
+        // ignore the message if it's not a MuseumCaperState message
+        if (!(info instanceof MuseumCaperState)) return;
+
+        this.state = (MuseumCaperState) info;
+        makeMove();
 
         Log.i("computer player", "receiving");
+    }
+    private void makeMove(){
+        GameAction action = null;
+
+        if(this.playerNum != state.getCurrentPlayer()){
+            return;
+        }
+        if(state.getIsThiefTurn()){
+            return;
+        }
+
+        try{
+            Thread.sleep(200);
+        }
+        catch(Exception e){
+            //do nothing
+        }
+
+        if(rng.nextInt(2) == 0){
+            int dir = rng.nextInt(4);
+
+            if (dir == 0){
+                action = new MuseumCaperMoveAction(this, 0, 1);
+            }
+            else if (dir == 1){
+                action = new MuseumCaperMoveAction(this, 0, -1);
+            }
+            else if (dir == 2){
+                action = new MuseumCaperMoveAction(this, 1, 0);
+            }
+            else{
+                action = new MuseumCaperMoveAction(this, -1, 0);
+            }
+        }
+        else{
+            if (rng.nextInt(1) == 0 && !state.getGuardActionUsed()){
+                action = new MuseumCaperUseEyesAction(this, getGuardPosition()[1], getGuardPosition()[0]);
+            }
+        }
+
+        if(state.getMoveCount() == 0 || state.getGuardActionUsed()){
+            action = new MuseumCaperEndTurnAction(this);
+        }
+
+        if (action != null) {
+            game.sendAction(action);
+        }
+    }
+
+    private int[] getGuardPosition(){
+        if(playerNum == 1){
+            return new int[]{
+                    state.getGuardOneLocation().get(1),
+                    state.getGuardOneLocation().get(0)
+            };
+        } else if(playerNum == 2) {
+            return new int[]{
+                    state.getGuardTwoLocation().get(1),
+                    state.getGuardTwoLocation().get(0)
+            };
+        } else if(playerNum == 3) {
+            return new int[]{
+                    state.getGuardThreeLocation().get(1),
+                    state.getGuardThreeLocation().get(0)
+            };
+        }
+        return new int[]{0,0};
     }
 }
